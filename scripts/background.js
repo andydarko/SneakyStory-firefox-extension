da59ea84b25857837f781ccffa3aedbae9a0a080
andydarko <80250074+andydarko@users.noreply.github.com>
2022-02-11 19:45:51 +0100

    Add files via upload

---
+++ scripts/background.js
@@ -0,0 +1,25 @@
+let toggle = false;
+
+browser.browserAction.onClicked.addListener(buttonClicked);
+
+function buttonClicked(){
+
+  if (toggle == false){
+    toggle = true;
+    browser.browserAction.setIcon({path: "icons/19_on.png"});
+  }else{
+    toggle = false;
+    browser.browserAction.setIcon({path: "icons/19_off.png"});
+  }
+
+  blocker(toggle);
+}
+
+function blocker(){
+  browser.webRequest.onBeforeRequest.addListener(function (e){
+    return {cancel: toggle};
+  }, {
+    types: ["xmlhttprequest"],
+    urls: ["https://i.instagram.com/api/v1/stories/reel/seen"],
+  },["blocking"]);
+}
