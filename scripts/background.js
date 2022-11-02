2beeaf56a6cc20d029be2b930ec262570f2aa673
andydarko <80250074+andydarko@users.noreply.github.com>
2022-11-02 06:10:32 +0100

    Endpoint changed
    From 1.instagram.com to www.instagram.com

--- scripts/background.js
+++ scripts/background.js
@@ -20,6 +20,6 @@ function blocker(){
     return {cancel: toggle};
   }, {
     types: ["xmlhttprequest"],
-    urls: ["https://i.instagram.com/api/v1/stories/reel/seen"],
+    urls: ["https://www.instagram.com/api/v1/stories/reel/seen"],
   },["blocking"]);
 }
